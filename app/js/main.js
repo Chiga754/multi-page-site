871b568999a2f31829138f18170c105dd3519feb
Chiga754 <a.chigarev96@gmail.com>
2021-11-02 09:06:15 +0500

    Верстка левого блока на странице blog

--- app/js/main.js
+++ app/js/main.js
@@ -22,7 +22,12 @@ $(function () {
         slidesToShow: 10,
         slidesToScroll: 10,
     });
-
+    
+    $('.article-slider__box').slick({
+        arrows: true,
+        prevArrow: '<button type="button" class="article-slider-arrow article-slider-arrowleft"><img src="images/blog/blog-slider-icon-left.svg" alt="icon"></button>',
+        nextArrow: '<button type="button" class="article-slider-arrow article-slider-arrowright"><img src="images/blog/blog-slider-icon-right.svg" alt="icon"></button>',
+    });
 
     // Mixit Up
     const mixer = mixitup('.gallery__inner', {
